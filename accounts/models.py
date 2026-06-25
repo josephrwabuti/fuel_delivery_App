331cf8315ea49066024f7762629c1ae1dfdfd0f9
josephrwabuti <josephrwabuti@gmail.com>
2026-06-25 08:06:25 +0300

    Created providers dashboard

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
-from django.db import models
-from django.core.validators import RegexValidator
-from django.contrib.auth.models import User
-
 # ================= VALIDATORS FIRST =================
 phone_validator = RegexValidator(
     regex=r'^\d{9}$',
@@ -24,23 +20,32 @@ business_license_validator = RegexValidator(
 
 # ================= MODEL =================
 class Profile(models.Model):
+
+    ROLE_CHOICES = [
+        ("customer", "Customer"),
+        ("provider", "Provider"),
+        ("driver", "Driver"),
+        ("admin", "Admin"),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    phone = models.CharField(
-        max_length=9,
-        validators=[phone_validator]
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default="customer"
     )
+
+    phone = models.CharField(max_length=9)
 
     license_number = models.CharField(
         max_length=11,
-        validators=[driver_license_validator],
         null=True,
         blank=True
     )
 
     license_no = models.CharField(
         max_length=12,
-        validators=[business_license_validator],
         null=True,
         blank=True
     )
