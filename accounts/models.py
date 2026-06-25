82521a4151ee30b5ad8ff9abc2ad37c49e7d7023
josephrwabuti <josephrwabuti@gmail.com>
2026-06-25 13:20:24 +0300

    fixed chnage

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,8 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
-<<<<<<< HEAD
-# ================= VALIDATORS FIRST =================
-=======
 # ================= VALIDATORS =================
 
->>>>>>> feature/driversdashboard
 phone_validator = RegexValidator(
     regex=r'^\d{9}$',
     message="Phone number must be exactly 9 digits."
@@ -34,9 +30,6 @@ class Profile(models.Model):
         ("admin", "Admin"),
     ]
 
-<<<<<<< HEAD
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-=======
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE
@@ -47,7 +40,6 @@ class Profile(models.Model):
         choices=ROLE_CHOICES,
         default="customer"
     )
->>>>>>> feature/driversdashboard
 
     role = models.CharField(
         max_length=20,
@@ -55,31 +47,17 @@ class Profile(models.Model):
         default="customer"
     )
 
-<<<<<<< HEAD
-    phone = models.CharField(max_length=9)
-
-    license_number = models.CharField(
-        max_length=11,
-        null=True,
-        blank=True
-=======
     # Driver License (Driver only)
     license_number = models.CharField(
         max_length=11,
         validators=[driver_license_validator],
         blank=True,
         null=True
->>>>>>> feature/driversdashboard
     )
 
     # Business License (Provider only)
     license_no = models.CharField(
         max_length=12,
-<<<<<<< HEAD
-        null=True,
-        blank=True
-    )
-=======
         validators=[business_license_validator],
         blank=True,
         null=True
@@ -87,4 +65,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.get_full_name()} ({self.role})"
->>>>>>> feature/driversdashboard
