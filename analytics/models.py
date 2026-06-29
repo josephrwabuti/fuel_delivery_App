efc2ccbeb402adda7bd70b9b9e86db925fb38777
josephrwabuti <josephrwabuti@gmail.com>
2026-06-29 07:41:16 +0300

    Backup before reseting database

--- analytics/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class DailyReport(models.Model):
-    station = models.ForeignKey("orders.Station", on_delete=models.CASCADE)
-    date = models.DateField()
-
-    total_orders = models.IntegerField(default=0)
-    total_revenue = models.DecimalField(max_digits=12, decimal_places=2, default=0)
